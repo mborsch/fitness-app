5c52c0bb0fa5097be8d9a17bb3f28058de49a5ce
mborsch <matthewmborschnack@gmail.com>
2022-08-14 21:40:00 -0500

    added youtube API

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,18 @@
 export const exerciseOptions = {
   method: "GET",
-  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+
   headers: {
     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
 

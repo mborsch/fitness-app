857a5154d03f29a0d23a83a261014d912db1e5c2
mborsch <matthewmborschnack@gmail.com>
2022-08-14 21:41:43 -0500

    added footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/ICO.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#ffdddd">
+      <Stack gap="40px" alignItems="center" px="40px" pt="40px">
+        <img src={Logo} alt="logo" width="50px" height="40px" />
+        <Typography variant="h5" pb="40px">
+          Made by Matt Borschnack for ICO Fitness
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;

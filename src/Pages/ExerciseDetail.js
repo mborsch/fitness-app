731933cd57e3dcb179a2575a52bb0b6a10434734
mborsch <matthewmborschnack@gmail.com>
2022-08-14 19:07:31 -0500

    exercise card and exercises for home page.

---
+++ src/Pages/ExerciseDetail.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+const ExerciseDetail = () => {
+  return <div>ExerciseDetail</div>;
+};
+
+export default ExerciseDetail;

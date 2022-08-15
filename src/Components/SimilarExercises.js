741be3ed2d1d244e17eccf5b3dc29b06012de967
mborsch <matthewmborschnack@gmail.com>
2022-08-14 21:41:07 -0500

    implemented similar exercise scrollbar

---
+++ src/Components/SimilarExercises.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollBar from "./HorizontalScrollBar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography variant="h4" mb="10px">
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }} mb="20px">
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+
+      <Typography variant="h4" mb="10px">
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }} mb="20px">
+        {equipmentExercises.length ? (
+          <HorizontalScrollBar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default SimilarExercises;
